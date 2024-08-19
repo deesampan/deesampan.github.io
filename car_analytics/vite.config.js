bf0a006d3f2afcf288b373fb5c6d50ca5cb3a9ab
deesampan <76961599+deesampan@users.noreply.github.com>
2024-08-19 21:11:32 +0700

    comit

--- car_analytics/vite.config.js
+++ car_analytics/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "car_analytics",
+  base : "/car_analytics/",
   plugins: [react()],
 })
