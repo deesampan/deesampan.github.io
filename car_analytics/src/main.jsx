ca07cf08e3bbcb3f4b3a155058eec88b939eae4b
deesampan <76961599+deesampan@users.noreply.github.com>
2024-08-20 09:58:23 +0700

    Update main.jsx

--- car_analytics/src/main.jsx
+++ car_analytics/src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter,RouterProvider,Route,Link } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Highlighted from './Highlighted';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:(
+      <Dashboard />
+    )
+  },
+  {
+    path:"highlighted",
+    element:(
+      <Highlighted />
+    )
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
