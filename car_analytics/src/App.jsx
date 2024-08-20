e55c6bcaca75daec04a73cc05c31edf03e71038c
deesampan <76961599+deesampan@users.noreply.github.com>
2024-08-20 09:51:26 +0700

    Update App.jsx

--- car_analytics/src/App.jsx
+++ car_analytics/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Dashboard from './Dashboard';
 import Highlighted from './Highlighted';
@@ -8,25 +8,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-// Define your routes
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/highlighted",
-    element: <Highlighted />,
-  },
-  {
-    path: "*",
-    element: <Dashboard />,  // Redirect to Dashboard for any other paths
-  },
-]);
-
 function App() {
   return (
-    <RouterProvider router={router}>
+    <Router basename="/car_analytics">
       <Container fluid>
         <Row>
           <Col>
@@ -34,12 +18,16 @@ function App() {
           </Col>
           <Col>
             <div style={{ position: 'absolute', top: '0', left: '200px', right: '0', padding: '20px', backgroundColor: 'white' }}>
-              <RouterProvider router={router} />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/highlighted" element={<Highlighted />} />
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
             </div>
           </Col>
         </Row>
       </Container>
-    </RouterProvider>
+    </Router>
   );
 }
 
